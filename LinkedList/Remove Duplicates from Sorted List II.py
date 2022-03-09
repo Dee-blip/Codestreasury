27c157062c8c385073c9c1c375b229db6a3c23ee
Deepak Chyaunal <52254018+Dee-blip@users.noreply.github.com>
2022-03-10 02:32:43 +0530

    Create Remove Duplicates from Sorted List II.py

---
+++ LinkedList/Remove Duplicates from Sorted List II.py
@@ -0,0 +1,14 @@
+  def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy =ListNode(-1,head) #dummy node which connects to head of the given linkedlist
+        prev=dummy     #we are pointing with the prev to the linkedlist
+        while head :  #till the end of the linked list
+            if head.next and head.val==head.next.val: # we are checking the element if head next element is present and head.val and head.next.val are same or not
+                while head.next and head.val ==head.next.val:# keep iterating until we didn't get ay different element
+                    head=head.next    #keep traversing 
+                prev.next=head.next # if we get then make a connection 
+            else:
+                prev= prev.next #otherwise not getting the equal element so keep move the prev and head pointer
+                
+            head=head.next
+        return dummy.next
+        
